bcc2d02f0319305ba17dda173651264cf9803fa0
Arnod Wamae <arnoldwamae2@gmail.com>
2023-01-28 11:50:25 +0300

    Bundled Vite

--- resources/views/filament/pages/configurations.blade.php
+++ resources/views/filament/pages/configurations.blade.php
@@ -1,47 +1,47 @@
 <x-filament::page>
 
-    <div class="my-2 grid grid-cols-12">
+    <div class="my-2 grid grid-cols-12 rounded-md border-b-2 border-b-danger-500">
         <label class="col-span-12 md:col-span-4">Landing Page Headline</label>
         <div class="col-span-12 md:col-span-8">
             <input class="px-2 py-2 w-full" wire:model="landing_page_headline">
         </div>
 
     </div>
-    <div class="my-2 grid grid-cols-12">
+    <div class="my-2 grid grid-cols-12 rounded-md border-b-2 border-b-danger-500">
         <label class="col-span-12 md:col-span-4">Landing Page Sub Title</label>
         <div class="col-span-12 md:col-span-8">
             <input class="px-2 py-2 w-full" wire:model="landing_page_sub_headline">
         </div>
 
     </div>
-    <div class="my-2 grid grid-cols-12">
+    <div class="my-2 grid grid-cols-12 rounded-md border-b-2 border-b-danger-500">
         <label class="col-span-12 md:col-span-4">Landing Page Tagline</label>
         <div class="col-span-12 md:col-span-8">
             <input class="px-2 py-2 w-full" wire:model="landing_page_tagline">
         </div>
 
     </div>
-    <div class="my-2 grid grid-cols-12">
+    <div class="my-2 grid grid-cols-12 rounded-md border-b-2 border-b-danger-500">
         <label class="col-span-12 md:col-span-4">Blog Page Headline</label>
         <div class="col-span-12 md:col-span-8">
             <input class="px-2 py-2 w-full" wire:model="blog_page_headline">
         </div>
 
     </div>
-    <div class="my-2 grid grid-cols-12">
+    <div class="my-2 grid grid-cols-12 rounded-md border-b-2 border-b-danger-500">
         <label class="col-span-12 md:col-span-4">Blog Page Sub Headline</label>
         <div class="col-span-12 md:col-span-8">
             <input class="px-2 py-2 w-full" wire:model="blog_page_sub_headline">
         </div>
 
     </div>
-    <div class="my-2 grid grid-cols-12">
+    <div class="my-2 grid grid-cols-12 rounded-md border-b-2 border-b-danger-500">
         <label class="col-span-12 md:col-span-4">Blog Page Tagline</label>
         <div class="col-span-12 md:col-span-8">
             <input class="px-2 py-2 w-full" wire:model="blog_page_tagline">
         </div>
     </div>
-    <div class="my-2 grid grid-cols-12">
+    <div class="my-2 grid grid-cols-12 rounded-md border-b-2 border-b-danger-500">
         <label class="col-span-12 md:col-span-4">Slogan</label>
         <div class="col-span-12 md:col-span-8">
             <input class="px-2 py-2 w-full" wire:model="slogan">
